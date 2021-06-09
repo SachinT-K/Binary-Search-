dc3c7eb7cf7d9a7553a07b7f8fc66d09b5d19b10
sachintk 97 <75876472+SachinT-K@users.noreply.github.com>
2021-06-09 12:44:32 +0000

    Readable

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ def binarySearch(arr,target):
   left = 0
   right = len(arr)-1
 
-  while(left <= right):
+  while( left <= right ):
     mid = (left+right)//2
 
-    if(arr[mid]==target):
+    if( arr[mid]==target ):
       return mid
 
-    elif(arr[mid]<target):
+    elif( arr[mid]<target ):
       left = mid+1
 
-    elif(arr[mid]>target):
+    elif( arr[mid]>target ):
       right = mid-1
   return -1
 
